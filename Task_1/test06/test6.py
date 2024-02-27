1a667dbd769c9091d675377485446da8cc4fe2db
Boyceing <boyceing@student.21-school.ru>
2024-02-27 10:28:40 +0000

    basic done

---
+++ Task_1/test06/test6.py
@@ -0,0 +1,39 @@
+import os
+import time 
+import stat
+import shutil
+
+total, used, free = shutil.disk_usage("/")
+
+SIX_MONTHS = 15780000
+
+size = 0
+
+dir_name = input('provide the dirname: ')
+size_limit = int(input('provide the size limit in Kb: '))
+filepaths = []
+
+for path, dirs, files in os.walk(dir_name):
+    for f in files:
+        fp = os.path.join(path, f)
+        filepaths.append(fp)
+        try:
+            size += os.path.getsize(fp)
+        except:
+            continue
+
+print(f"Free: {round(free / (2**30), 2)} GiB")
+print(f'{dir_name} size is {size} bytes')
+
+def file_age_in_seconds(pathname):
+    return time.time() - os.stat(pathname)[stat.ST_MTIME]
+
+
+for file in filepaths:
+    age = file_age_in_seconds(file)
+    size = os.stat(file).st_size / 2**10
+
+    if age > SIX_MONTHS and size > size_limit:
+        print(f'Deleting {file}...')
+        os.remove(file)
+        
